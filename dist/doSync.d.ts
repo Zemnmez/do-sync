60c00f3b02afa8b1823d9ff216892fb90e1abc3d
thomas shadwell <thomas.shadwell@googlemail.com>
2020-04-06 00:13:22 +0100

    add coverage minimum

--- dist/doSync.d.ts
+++ dist/doSync.d.ts
@@ -1,11 +1,10 @@
-declare type JSONPrimitive = string | number | boolean | null;
-declare type JSONValue = JSONPrimitive | JSONObject | JSONArray;
-declare type JSONObject = {
-    [member: string]: JSONValue;
-};
-declare type JSONArray = JSONValue[];
+export declare type JSONPrimitive = string | number | boolean | null;
+export declare type JSONValue = JSONObject | JSONArray | JSONPrimitive;
+export interface JSONObject extends Record<string, JSONValue> {
+}
+export declare type JSONArray = JSONValue[];
 export declare type Value = JSONValue;
-export declare type AsyncFn<I extends Value[] = Value[], O extends Value = Value> = (...v: I) => Promise<O>;
-export declare const doSync: <I extends Value[], O extends Value>(f: AsyncFn<I, O>) => (...ip: I) => O;
+export declare type AsyncFn<I extends Value[], O extends Value> = (...v: I) => Promise<O>;
+export declare const doSync: <IK extends Value, I extends IK[], O extends Value>(f: AsyncFn<I, O>) => (...ip: I) => O;
 export default doSync;
 //# sourceMappingURL=doSync.d.ts.map
