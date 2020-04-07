15bfefe0da807b1e41a5372fcbfb4683130189b6
thomas shadwell <thomas.shadwell@googlemail.com>
2020-04-07 19:13:39 +0100

    update dist

--- dist/doSync.d.ts
+++ dist/doSync.d.ts
@@ -3,8 +3,24 @@ export declare type JSONValue = JSONObject | JSONArray | JSONPrimitive;
 export interface JSONObject extends Record<string, JSONValue> {
 }
 export declare type JSONArray = JSONValue[];
+/**
+ * Value represents data that can safely be input to,
+ * or returned from a doSync() function.
+ */
 export declare type Value = JSONValue;
+/**
+ * An AsyncFn can be used with doSync().
+ */
 export declare type AsyncFn<I extends Value[], O extends Value> = (...v: I) => Promise<O>;
-export declare const doSync: <IK extends Value, I extends IK[], O extends Value>(f: AsyncFn<I, O>) => (...ip: I) => O;
+/**
+ * doSync returns a synchronous version of certian
+ * special asynchronous functions by extracting them
+ * and running them as a synchronous node subprocess.
+ *
+ * The input and output types of the function must be serializible
+ * to JSON, and the function must not reference any parent
+ * scopes (i.e. file-defined variables) to function.
+ */
+export declare const doSync: <I extends Value[], O extends Value>(f: AsyncFn<I, O>) => (...ip: I) => O;
 export default doSync;
 //# sourceMappingURL=doSync.d.ts.map
--- dist/doSync.js
+++ dist/doSync.js
@@ -11,15 +11,24 @@ const main = async () => {
 
 main().catch(e => console.error(e));
 `;
+/**
+ * doSync returns a synchronous version of certian
+ * special asynchronous functions by extracting them
+ * and running them as a synchronous node subprocess.
+ *
+ * The input and output types of the function must be serializible
+ * to JSON, and the function must not reference any parent
+ * scopes (i.e. file-defined variables) to function.
+ */
 exports.doSync = fn => (...ip) => {
     const proc = cross_spawn_1.default.sync('node', ['-'], {
         input: gen(ip, fn)
     });
-    if (proc.error)
-        throw Error;
     const stderr = proc.stderr.toString('utf-8').trim();
     if (stderr)
         console.error(stderr);
+    if (proc.error)
+        throw Error;
     return JSON.parse(proc.stdout.toString('utf-8'));
 };
 exports.default = exports.doSync;
