78c33f4378cbbb7e1915006b6474beb39fc486de
zemnmez <zemnmez@googlemail.com>
2020-12-26 01:20:44 +0000

    ignore coverage

--- dist/doSync.d.ts
+++ dist/doSync.d.ts
@@ -1,3 +1,5 @@
+/// <reference types="node" />
+import { SpawnSyncOptions } from 'child_process';
 export declare type JSONPrimitive = string | number | boolean | null | undefined;
 export declare type JSONValue = JSONObject | JSONArray | JSONPrimitive;
 export interface JSONObject extends Record<string, JSONValue> {
@@ -12,6 +14,8 @@ export declare type Value = JSONValue;
  * An AsyncFn can be used with doSync().
  */
 export declare type AsyncFn<I extends Value[], O extends Value> = (...v: I) => Promise<O>;
+export interface DoSyncOptions extends SpawnSyncOptions {
+}
 /**
  * doSync returns a synchronous version of certian
  * special asynchronous functions by extracting them
@@ -21,6 +25,6 @@ export declare type AsyncFn<I extends Value[], O extends Value> = (...v: I) => P
  * to JSON, and the function must not reference any parent
  * scopes (i.e. file-defined variables) to function.
  */
-export declare const doSync: <I extends Value[], O extends Value>(f: AsyncFn<I, O>) => (...ip: I) => O;
+export declare const doSync: <I extends Value[], O extends Value>(f: AsyncFn<I, O>, opts?: DoSyncOptions) => (...ip: I) => O;
 export default doSync;
 //# sourceMappingURL=doSync.d.ts.map
--- dist/doSync.js
+++ dist/doSync.js
@@ -3,13 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.doSync = void 0;
 const cross_spawn_1 = __importDefault(require("cross-spawn"));
 const gen = (input, fn) => `
 const main = async () => {
-    console.log(JSON.stringify(await (${fn})(...${JSON.stringify(input)})));
+    console.log(JSON.stringify({ type: "success", value: await (${fn})(...${JSON.stringify(input)}) }));
 }
 
-main().catch(e => console.error(e));
+main().catch(e => console.log(JSON.stringify({ type: "failure", value: e })));
 `;
 /**
  * doSync returns a synchronous version of certian
@@ -20,16 +21,21 @@ main().catch(e => console.error(e));
  * to JSON, and the function must not reference any parent
  * scopes (i.e. file-defined variables) to function.
  */
-exports.doSync = fn => (...ip) => {
+exports.doSync = (fn, { maxBuffer = 1000 * 1024 * 1024, ...etc } = {}) => (...ip) => {
     const proc = cross_spawn_1.default.sync('node', ['-'], {
-        input: gen(ip, fn)
+        input: gen(ip, fn),
+        maxBuffer,
+        ...etc
     });
     const stderr = proc.stderr.toString('utf-8').trim();
     if (stderr)
         console.error(stderr);
     if (proc.error)
         throw proc.error;
-    return JSON.parse(proc.stdout.toString('utf-8'));
+    const rsp = JSON.parse(proc.stdout.toString('utf-8'));
+    if (rsp.type == "failure")
+        throw rsp.value;
+    return rsp.value;
 };
 exports.default = exports.doSync;
 //# sourceMappingURL=doSync.js.map
